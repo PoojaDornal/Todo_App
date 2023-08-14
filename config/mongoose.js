96ba1cab4c6fef86221ee6ef18c516095a78e914
PoojaDornal <37832052+PoojaDornal@users.noreply.github.com>
2023-08-14 19:31:17 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,4 +6,4 @@ db.on('error',console.error.bind(console,'erroe connecting to db'));
 // up and running then message
 db.once('open',function(){
     console.log('Success fully connected to the database')
-})
+});
